e30ed872c15c045fec1373e77a94bd681125a7b7
Laxmi Kandel <laxmikandel97@gmail.com>
2020-01-09 23:21:54 -0500

    created heehaw.html file which  shows the discription of program

---
+++ heehaw.js
@@ -0,0 +1,12 @@
+var i;
+for (i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("Hee Haw!");
+    } else if (i % 3 == 0) {
+        console.log("Hee!");
+    } else if (i % 5 == 0) {
+        console.log("Haw!");
+    } else {
+        console.log(i);
+    }
+}
